3b4c683435f037b9825962d6742dc87bd81580c0
Michael <mike.alex.martinez@gmail.com>
2018-08-07 12:07:19 +0100

    finished text nofif

--- serverless-reminders/handler.js
+++ serverless-reminders/handler.js
@@ -1,16 +1,85 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+module.exports.sendReminderDaily = (event, context, callback) => {
+
+  const AWS = require('aws-sdk');
+  AWS.config.update({region:'eu-west-1'});
+
+  const ses = new AWS.SES();
+  const fs = require('fs');
+
+  const emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+  const toAndFromAddress = 'mike.alex.martinez@gmail.com';
+
+  const params = {
+    Destination: {
+      ToAddresses: [toAndFromAddress]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: emailHtml
+        },
+        Text: {
+          Charset: 'UTF-8',
+          Data: 'Remember to continure helping the Woof Garden in your Pluralsight course!'
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Woof Garden Reminder'
+      }
+    },
+    ReplyToAddresses: [toAndFromAddress],
+    Source: toAndFromAddress,
   };
 
-  callback(null, response);
+  ses.sendEmail(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else callback(null, data);
+  });
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.sendReminderWeekend = (event, context, callback) => {
+
+  const AWS = require('aws-sdk');
+  AWS.config.update({region:'eu-west-1'});
+
+  const ses = new AWS.SES();
+  const fs = require('fs');
+
+  const emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+  const toAndFromAddress = 'mike.alex.martinez@gmail.com';
+
+  const params = {
+    Destination: {
+      ToAddresses: [toAndFromAddress]
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: emailHtml
+        },
+        Text: {
+          Charset: 'UTF-8',
+          Data: 'Here\'s a weekend Remember that puppies are adorable!!'
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Woof Garden Reminder'
+      }
+    },
+    ReplyToAddresses: [toAndFromAddress],
+    Source: toAndFromAddress,
+  };
+
+  ses.sendEmail(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else callback(null, data);
+  });
 };
